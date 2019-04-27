b1f43b1e92a5a35a81ef161d1bb5a7235223ef47
Maasa Kono <konomaasa@gmail.com>
2019-04-27 21:11:04 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,12 +3,11 @@ def create_an_empty_array
 end
 
 def create_an_array
-  [1, 2, 3, 4]
+  array = [1, 2, 3, 4]
 end
 
 def add_element_to_end_of_array(array, element)
-  [1, 2, 3, 4]
-  << 5
+  array.push(5)
 end
 
 def add_element_to_start_of_array(array, element)
