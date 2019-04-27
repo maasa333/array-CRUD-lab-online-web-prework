a4210fa7bada806b01f5817326b326dd370abb41
Maasa Kono <konomaasa@gmail.com>
2019-04-27 21:02:52 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,7 +3,7 @@ def create_an_empty_array
 end
 
 def create_an_array
-  my_pets ["Inky", "Rocky", "Looc", "Fluffy"]
+  my_pets = ["Inky", "Rocky", "Looc", "Fluffy"]
 end
 
 def add_element_to_end_of_array(array, element)
