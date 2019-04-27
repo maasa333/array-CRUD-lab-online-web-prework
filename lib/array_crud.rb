8ff0de2c39a5558fd9dc668569b589b361f72f20
Maasa Kono <konomaasa@gmail.com>
2019-04-27 20:58:01 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,11 +3,11 @@ def create_an_empty_array
 end
 
 def create_an_array
-
+  ["Ini", "Mini", "Maini", "Mo"]
 end
 
 def add_element_to_end_of_array(array, element)
-
+  ["Ini", "Mini", "Maini", "Mo"]
 end
 
 def add_element_to_start_of_array(array, element)
